0b084a75ab2da9fabfd9dd05adabaa1e685d9ded
arquimedesferreira <ferrerasousa19.88@gmail.com>
2018-07-10 23:29:40 -0300

    atualizando  Agenda Domani Teste

---
+++ Agenda.Domani.Test/Entity/ContactTest.cs
@@ -0,0 +1,27 @@
+﻿using Agenda.Domain.Enum;
+using Agenda.Domain.ValueObject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agenda.Domani.Test.Entity
+{
+    [TestClass]
+    class ContactTest
+    {
+        [TestMethod]
+        public void TestaSeContatoPodeSerTipoNullo()
+        {
+            var contato = new Contact(EContactType.CELULAR, "12312312");
+            Assert.IsTrue(contato.Type == 0);
+        }
+
+        [TestMethod]
+        public void TesteSeContatoPodeTerValorVazio()
+        {
+            var contato = new Contact(EContactType.EMAIL, "");
+            Assert.IsFalse(contato.Type == 3);
+        }
+    }
+}
